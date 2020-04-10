ce5e8821e707e1286a038102179edc2d5a0fd446
Utarn <utarn@yahoo.com>
2020-04-10 09:24:29 +0700

    Finish Project

--- Program.cs
+++ Program.cs
@@ -6,7 +6,30 @@ namespace pattern_memento
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var stateManager = new CareTaker<Editor>();
+            var editor = new Editor("T1", "Text 1", 14, "TH SarabunPSK");
+            Console.WriteLine(editor);
+            var state1 = editor.CreateState();
+            stateManager.PushMemento(state1);
+            editor = new Editor("T2", "Text 2", 15, "TH SarabunPSK");
+            Console.WriteLine(editor);
+            stateManager.PushMemento(editor.CreateState());
+            editor = new Editor("T3", "Text 3", 16, "TH Sarabun New");
+            Console.WriteLine(editor);
+            var undoState = stateManager.Undo();
+            editor.RestoreState(undoState);
+            Console.WriteLine(editor);
+            editor.RestoreState(stateManager.Undo());
+            Console.WriteLine(editor);
+            editor.RestoreState(stateManager.Redo());
+            Console.WriteLine(editor);
+            editor = new Editor("T3", "Text 3", 16, "TH Sarabun New");
+            Console.WriteLine(editor);
+            stateManager.PushMemento(editor.CreateState());
+            editor = new Editor("T4", "Text 4", 17, "TH Sarabun New");
+            Console.WriteLine(editor);
+            editor.RestoreState(stateManager.Undo());
+            Console.WriteLine(editor);
         }
     }
 }
