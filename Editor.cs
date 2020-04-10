5f0324597c14153dd3c54d7d0dec5d8159f12e44
Utarn <utarn@yahoo.com>
2020-04-10 08:42:39 +0700

    Project starter

---
+++ Editor.cs
@@ -0,0 +1,10 @@
+namespace pattern_memento
+{
+    public class Editor
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public int FontSize { get; set; }
+        public string FontFace { get; set; }
+    }
+}
